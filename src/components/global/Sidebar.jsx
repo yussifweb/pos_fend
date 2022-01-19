6c511a4ff2506c0e0bc19c22c309d8c0eac1e3e8
Yussif <djiceman.gh@gmail.com>
2022-01-19 09:43:36 +0000

    remove currentstore and role at logout

--- src/components/global/Sidebar.jsx
+++ src/components/global/Sidebar.jsx
@@ -120,6 +120,8 @@ function Sidebar(props) {
             if (res.data.status === 200) {
                 localStorage.removeItem('auth_token', res.data.token);
                 localStorage.removeItem('auth_name', res.data.username);
+                localStorage.removeItem('currentStore');
+                localStorage.removeItem('role');
                 swal("Success", res.data.message, "success");
                 history.push('/login');
             } else {
