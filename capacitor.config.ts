0486005a5e138746be7c1be6788d78de9c139146
Yussif <djiceman.gh@gmail.com>
2022-03-20 22:37:27 +0000

    update

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.amspos.app',
+  appName: 'AMS POS',
+  webDir: 'build',
+  bundledWebRuntime: false
+};
+
+export default config;
